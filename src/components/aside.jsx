f1d10faa7db21ad8a9e3c8af06921543ed8627e9
eymen iron <96985158+eymen-iron@users.noreply.github.com>
2024-02-07 23:16:03 +0300

    Version 1.0.4.1

--- src/components/aside.jsx
+++ src/components/aside.jsx
@@ -9,7 +9,7 @@ const Aside = ({ handle, data, setLang, lang }) => {
       <Filters handle={handle} lang={lang} />
       <div className="relative mt-auto w-full">
         <a
-          href="https://www.instagram.com/simeon_eymen"
+          href="https://github.com/eymen-iron"
           target="_blank"
           className="text-start text-purple-400"
         >
